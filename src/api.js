62df02203ae5b85601fee98d17cc9776e765aa03
Nitin Singh <111803320+virat-017@users.noreply.github.com>
2023-02-22 16:12:51 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -59,7 +59,7 @@ router.get("/exercises", (req, res) => {
 });
 
 
-app.use(`/api`, router);
+app.use(`/.netlify/functions/api`, router);
 
 module.exports = app;
 module.exports.handler = serverless(app);
